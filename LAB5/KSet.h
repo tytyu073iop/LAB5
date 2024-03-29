3ecf5db2f8d239853d3da845b666abf5e3049355
tytyu073iop <iiya.ps4.com@gmail.com>
2024-03-29 18:06:31 +0300

    Final

--- LAB5/KSet.h
+++ LAB5/KSet.h
@@ -4,7 +4,30 @@
 #include <set>
 #include <algorithm>
 
-class KSetIterator;
+template <typename T>
+class KSet;
+
+template <typename T>
+class KSetIterator {
+protected:
+	KSet<T>* set;
+	size_t index = 0;
+	KSetIterator();
+	KSetIterator(KSet<T>* set, size_t index);
+public:
+	KSetIterator(KSet<T>* set);
+	void forward();
+	void back();
+	T& get() const;
+	KSetIterator operator++();
+	KSetIterator operator--();
+	KSetIterator& operator++(int i);
+	KSetIterator& operator--(int i);
+	T& operator*();
+	bool operator==(const KSetIterator& other) const;
+	template <typename U>
+	friend class KSet;
+};
 
 template <typename T>
 class KSet {
@@ -22,14 +45,29 @@ public:
 	KSet<T> operator/(const KSet<T>& rhs) const;
 	KSet<T>& operator/=(const KSet<T>& rhs);
 	KSet<T>& operator>>(const T& rhs);
-	friend std::ostream& operator<<(std::ostream& os, const KSet<T>& rhs);
+	template <typename Q>
+	friend std::ostream& operator<<(std::ostream& os, const KSet<Q>& rhs);
 	size_t Size() const;
 	void Clear();
 	bool IsEmpty() const;
 	bool IsFull() const;
-	KSetIterator GetIterator();
+	KSetIterator<T> begin();
+	KSetIterator<T> end();
+	template <typename U>
 	friend class KSetIterator;
 };
+
+template <typename T>
+KSetIterator<T> KSet<T>::begin()
+{
+	return KSetIterator(this, 0);
+}
+
+template <typename T>
+KSetIterator<T> KSet<T>::end()
+{
+	return KSetIterator(this, v.size() - 1);
+}
 
 template <typename T>
 KSet<T>::KSet()
@@ -76,7 +114,7 @@ KSet<T>& KSet<T>::operator|=(const KSet<T>& rhs)
 template <typename T>
 KSet<T> KSet<T>::operator&(const KSet<T>& rhs) const
 {
-	std::vector<int> q, w1(v), w2(rhs.v);
+	std::vector<T> q, w1(v), w2(rhs.v);
 	std::sort(w1.begin(), w1.end());
 	std::sort(w2.begin(), w2.end());
 	for (auto i = w1.begin(), j = w2.begin(); i != w1.end() && j != w2.end();) {
@@ -101,7 +139,7 @@ KSet<T>& KSet<T>::operator&=(const KSet<T>& rhs)
 template<typename T>
 KSet<T> KSet<T>::operator/(const KSet<T>& rhs) const
 {
-	std::vector<int> q, w1(v), w2(rhs.v);
+	std::vector<T> q, w1(v), w2(rhs.v);
 	std::sort(w1.begin(), w1.end());
 	std::sort(w2.begin(), w2.end());
 	for (auto i = w1.begin(), j = w2.begin(); i != w1.end() && j != w2.end();) {
@@ -133,7 +171,7 @@ KSet<T>& KSet<T>::operator>>(const T& rhs)
 template<typename T>
 std::ostream& operator<<(std::ostream& os, const KSet<T>& rhs)
 {
-	for (const auto& i : rhs.v) {
+	for (const T& i : rhs.v) {
 		os << i << " ";
 	}
 	return os;
@@ -159,7 +197,77 @@ bool KSet<T>::IsFull() const {
 	return false;
 }
 
-template<typename T>
-KSetIterator KSet<T>::GetIterator() {
-	return KSetIterator(this);
+template <typename T>
+bool KSetIterator<T>::operator==(const KSetIterator& other) const
+{
+	return index == other.index && set == other.set;
+}
+
+template <typename T>
+KSetIterator<T>::KSetIterator()
+{
+}
+
+template <typename T>
+KSetIterator<T>::KSetIterator(KSet<T>* set) : set(set)
+{
+}
+
+template <typename T>
+KSetIterator<T>::KSetIterator(KSet<T>* set, size_t index) : set(set), index(index)
+{
+}
+
+template <typename T>
+T& KSetIterator<T>::get() const
+{
+	return set->v[index];
+}
+
+template <typename T>
+void KSetIterator<T>::forward()
+{
+	index++;
+}
+
+template <typename T>
+void KSetIterator<T>::back()
+{
+	index--;
+}
+
+template <typename T>
+KSetIterator<T> KSetIterator<T>::operator--()
+{
+	KSetIterator<T> temp = *this;
+	back();
+	return temp;
+}
+
+template <typename T>
+KSetIterator<T>& KSetIterator<T>::operator--(int i)
+{
+	back();
+	return *this;
+}
+
+template <typename T>
+KSetIterator<T> KSetIterator<T>::operator++()
+{
+	KSetIterator<T> temp = *this;
+	forward();
+	return temp;
+}
+
+template <typename T>
+KSetIterator<T>& KSetIterator<T>::operator++(int i)
+{
+	forward();
+	return *this;
+}
+
+template <typename T>
+T& KSetIterator<T>::operator*()
+{
+	return get();
 }
--- LAB5/program.cpp
+++ LAB5/program.cpp
@@ -1,68 +1,84 @@
-#include "iterator.h"
 #include "KSet.h"
 
-long find(Kset* set, int num) {
-    ksi it = set->GetIterator();
-    for (int i = 0;;i++) {
-        try {
-            if (it.get() == num) {
-                return i;
-            }
-            it.forward();
-        } catch (ksi::Exception) {
-            return -1;
+template <typename T>
+long find(KSet<T>* set, T num) {
+    auto it = set->begin();
+    for (long i = 0; it != set->end();it.forward()) {
+        if (it.get() == num) {
+            return i;
         }
+        i++;
     }
+    return -1;
 }
 
-long find2(Kset* set, int num) {
-    ksi it = set->GetIterator();
-    for (int i = 0;;i++) {
-        try {
-            if (*it == num) {
-                return i;
-            }
-            it++;
-        } catch (ksi::Exception) {
-            return -1;
+struct Psint {
+    int i;
+    bool operator==(const Psint& rhs) const {
+        return i == rhs.i;
+    }
+    bool operator<(const Psint& rhs) const {
+        return i < rhs.i;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Psint& p) {
+        return os << p.i;
+    }
+    Psint(int i) : i(i) {}
+};
+
+template <typename T>
+long find2(KSet<T>* set, T num) {
+    auto it = set->begin();
+    for (int i = 0; it != set->end();i++, it++) {
+        if (*it == num) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+template <typename T>
+void testKset() {
+    {
+        KSet<T> set({T(0),T(3),T(5),T(6),T(8),T(1)});
+        std::cout << find(&set, T(3)) << std::endl;
+        std::cout << find2(&set, T(3)) << std::endl;
+        std::cout << find(&set, T(4)) << std::endl;
+        // test inside
+        KSet<T> s1;
+        s1 >> 2 >> 5 >> 0;
+        std::cout << "s1: " << s1 << '\n';
+        std::cout << "s1 | set: " << (s1 | set) << '\n';
+        std::cout << "s1 & set: " << (s1 & set) << '\n';
+        std::cout << "s1 / set: " << (s1 / set) << '\n';
+        std::cout << "set.size(): " << set.Size() << '\n';
+        std::cout << "set.isempty(): " << set.IsEmpty() << '\n';
+        std::cout << "set.isfull(): " << set.IsFull() << '\n';
+        {
+            KSet<T> s2;
+            s2 >> 6;
+            std::cout << "s2: " << s2 << '\n';
+            s2 = s1;
+            std::cout << "s2 = s1: " << s2 << '\n';
+            std::cout << "s2 |= set: " << (s2 |= set) << '\n';
+            s2.Clear();
+            std::cout << "s2 after clear: " << s2 << '\n';
+        }
+        {
+            KSet<T> s2;
+            s2 = s1;
+            std::cout << "s2 &= set: " << (s2 &= set) << '\n';
+        }
+        {
+            KSet<T> s2;
+            s2 = s1;
+            std::cout << "s2 /= set: " << (s2 /= set) << '\n';
         }
     }
 }
 
 int main() {
-    KSet set({0,3,5,6,8,1});
-    std::cout << find(&set, 3) << std::endl;
-    std::cout << find2(&set, 3) << std::endl;
-    std::cout << find(&set, 4) << std::endl;
-    // test inside
-    KSet s1;
-    s1 >> 2 >> 5 >> 0;
-    std::cout << "s1: " << s1 << '\n';
-    std::cout << "s1 | set: " << (s1 | set) << '\n';
-    std::cout << "s1 & set: " << (s1 & set) << '\n';
-    std::cout << "s1 / set: " << (s1 / set) << '\n';
-    std::cout << "set.size(): " << set.Size() << '\n';
-    std::cout << "set.isempty(): " << set.IsEmpty() << '\n';
-    std::cout << "set.isfull(): " << set.IsFull() << '\n';
-    {
-        KSet s2;
-        s2 >> 6;
-        std::cout << "s2: " << s2 << '\n';
-        s2 = s1;
-        std::cout << "s2 = s1: " << s2 << '\n';
-        std::cout << "s2 |= set: " << (s2 |= set) << '\n';
-        s2.Clear();
-        std::cout << "s2 after clear: " << s2 << '\n';
-    }
-    {
-        Kset s2;
-        s2 = s1;
-        std::cout << "s2 &= set: " << (s2 &= set) << '\n';
-    }
-    {
-        Kset s2;
-        s2 = s1;
-        std::cout << "s2 /= set: " << (s2 /= set) << '\n';
-    }
+    testKset<int>();
+    testKset<Psint>();
     return 0;
 }
